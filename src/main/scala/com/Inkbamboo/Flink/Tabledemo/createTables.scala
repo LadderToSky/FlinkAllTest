03d31df7d7865884a76458598cd86f855f9c84a5
zlhao <hao_zl1104@163.com>
2019-02-26 15:43:16 +0800

    tableAPI测试代码

--- src/main/scala/com/Inkbamboo/Flink/Tabledemo/createTables.scala
+++ src/main/scala/com/Inkbamboo/Flink/Tabledemo/createTables.scala
@@ -1,5 +1,6 @@
 package com.Inkbamboo.Flink.Tabledemo
 
+import java.util
 import java.util.Properties
 
 import org.apache.flink.api.common.serialization.SimpleStringSchema
@@ -10,9 +11,10 @@ import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010
 import org.apache.flink.table.api.{Table, TableEnvironment, Types}
 import org.apache.flink.table.sinks.{CsvTableSink, TableSink}
+import org.apache.flink.types.Row
 import org.apache.kafka.common.config.ConfigDef.Type
-
-
+import scala.collection.JavaConversions._
+import  scala.collection.JavaConverters._
 
 /**
   * Created By InkBamboo
@@ -20,60 +22,70 @@ import org.apache.kafka.common.config.ConfigDef.Type
   * Calm Positive
   * Think Then Ask
   *
-  * desc:  官方api，上手测试tableApi
+  * desc:  tableAPI测试
   */
-object streamcreateTables {
-  import org.apache.flink.streaming.api.scala._
-  def main(args: Array[String]): Unit = {
-
-    val streamenv = StreamExecutionEnvironment.getExecutionEnvironment
-    val tableEnv = TableEnvironment.getTableEnvironment(streamenv)
-
-    val prop = new Properties()
-    prop.setProperty("bootstrap.servers", "192.168.183.135:9092")
-    prop.setProperty("zookeeper.connect", "192.168.183.135:2181")
-    prop.setProperty("group.id", "test")
-
-    val resstream = streamenv.addSource(new FlinkKafkaConsumer010[String]("wiki_test",new SimpleStringSchema(),prop))
-    val wordecount = resstream
-      .filter(x=>x.nonEmpty)
-      .flatMap(x=>x.split(" "))
-      .map(x=>((x,x.hashCode),1))
-      .keyBy(0)
-      .timeWindow(Time.seconds(10))
-      .sum(1)
-      .map(x=>(x._1._1,x._1._2,x._2))
-
-
-    val tbl = tableEnv.fromDataStream(wordecount)
-    tbl.printSchema()
-
-   tableEnv.registerTable("tbl_testOne",tbl)
-
-    val restbl =  tableEnv.sqlQuery("select count(1) from tbl_testOne")
-
-    streamenv.execute("tableApiTest")
-  }
-
-}
-
+/**
+  * 加载csv文件处理
+  * InkBamboo:测试通过
+  */
 object batchTable{
   import org.apache.flink.api.scala._
   def main(args: Array[String]): Unit = {
 
+    //初始化环境
     val batchenv = ExecutionEnvironment.getExecutionEnvironment
     val tableEnv = TableEnvironment.getTableEnvironment(batchenv)
 
-    val csvDataset  = new CsvTableSink("D:\\scala_workspace\\flinktest\\src\\main\\resources\\UserBehavior.csv")
-    val fieldNames = Array("a","b","c","d","e")
-    val fieldTypes:Array[TypeInformation[_]] = Array(Types.STRING,Types.STRING,Types.STRING,Types.STRING,Types.STRING)
-    tableEnv.registerTableSink("tbl_test_sink",fieldNames,fieldTypes,csvDataset)
+    //加载csv文件
+    val csvds:DataSet[(String,String,String,String,String)] = batchenv.readCsvFile("E:\\scala_workspace\\FlinkAllTest\\src\\main\\resources\\UserBehavior.csv")
 
-    val restbl = tableEnv.sqlQuery("select a,d,b from tbl_test_sink")
-    val writesink = new CsvTableSink("D:\\scala_workspace\\flinktest\\src\\main\\resources\\testtable")
+    //加载数据集
+   val tbl =  tableEnv.fromDataSet(csvds)
+     //为数据指定每列的名字。
+     .as("id,id2,id3,flag,number")
+    //注册成表
+    tableEnv.registerTable("testOne",tbl)
+   val restbl =  tableEnv.sqlQuery("select id from testOne order by id limit 1000")
 
-    restbl.writeToSink(writesink)
+    //部分数据输出
+    println("------------------------------------")
+    tableEnv.toDataSet[Row](restbl).print()
 
+    //结果数据集写入到外部存储系统
+    val writeSink = new CsvTableSink("E:\\scala_workspace\\FlinkAllTest\\src\\main\\resources\\res100")
+    restbl.writeToSink(writeSink)
     batchenv.execute("table_test")
   }
 }
+
+
+/**
+  * InkBamboo :测试通过数据正常输出
+  */
+object TableAPITest {
+  import org.apache.flink.api.scala._
+
+  def main(args: Array[String]): Unit = {
+    val arr = Array(new DataPackage(1,"a",2),new DataPackage(2,"b",3),new DataPackage(3,"c",4))
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+    val tblenv = TableEnvironment.getTableEnvironment(env)
+
+    val source =  env.fromCollection(arr)
+    val tbl1 = tblenv.fromDataSet(source).as("id,name,id2")   //as用逗号分割，重命名列名
+    tblenv.registerTable("dataPackage",tbl1)
+
+    val tblAPIResult = tblenv.scan("dataPackage").select("id,name,id2")
+    tblenv.toDataSet[Row](tblAPIResult).print()
+
+    //println("-----------------id--------"+tblAPIResult.toString())
+
+    env.execute()
+
+  }
+  def flatfun(): Unit ={
+
+  }
+}
+
+case class DataPackage(id:Int,name:String,id2:Int) extends Serializable
