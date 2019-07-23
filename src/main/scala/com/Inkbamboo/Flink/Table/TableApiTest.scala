dfdfd052340809682f88711cc8343a4540ff48aa
zlhao <hao_zl1104@163.com>
2019-07-23 22:21:05 +0800

    TableApi测试代码构建规范

--- src/main/scala/com/Inkbamboo/Flink/Table/TableApiTest.scala
+++ src/main/scala/com/Inkbamboo/Flink/Table/TableApiTest.scala
@@ -1,8 +1,10 @@
 package com.Inkbamboo.Flink.Table
 
+import org.apache.flink.core.fs.FileSystem.WriteMode
 import org.apache.flink.table.api.{Table, TableEnvironment, Types}
 import org.apache.flink.table.sinks.{CsvTableSink, TableSink}
 import org.apache.flink.types.Row
+import org.apache.flink.table.api.scala._
 
 /**
   * Created By InkBamboo
@@ -10,7 +12,7 @@ import org.apache.flink.types.Row
   * Calm Positive
   * Think Then Ask
   *
-  * desc:  tableAPI测试
+  * desc:  tableAPI源码中example样例测试
   */
 /**
   * 加载csv文件处理
@@ -25,23 +27,53 @@ object batchTable{
     val tableEnv = TableEnvironment.getTableEnvironment(batchenv)
 
     //加载csv文件
-    val csvds:DataSet[(String,String,String,String,String)] = batchenv.readCsvFile("E:\\scala_workspace\\FlinkAllTest\\src\\main\\resources\\UserBehavior.csv")
+    val csvds:DataSet[(String,String,String,String,String)] = batchenv.readCsvFile("E:\\workspace\\big_data\\FlinkAllTest\\src\\main\\resources\\UserBehavior.csv")
 
-    //加载数据集
-   val tbl =  tableEnv.fromDataSet(csvds)
-     //为数据指定每列的名字。
-     .as("id,id2,id3,flag,number")
-    //注册成表
+    /****************************************************
+      * 创建表 function One
+      */
+
+    tableEnv.registerDataSet("TestTwo",csvds,'id,'id2,'id3,'flag,'number)
+
+    tableEnv.sqlQuery("select id,id2,id3,flag,number from TestTwo")
+
+      /**此处的dataset需要指定typeinformation*/
+      .toDataSet[(String,String,String,String,String)]
+
+      .print()
+
+    /**
+      * 创建表 function Two
+      */
+    val tbl =  tableEnv.fromDataSet(csvds)
+
+      /**为数据指定每列的名字。*/
+      .as("id,id2,id3,flag,number")
+
+    /**注册成表*/
     tableEnv.registerTable("testOne",tbl)
-   val restbl =  tableEnv.sqlQuery("select id from testOne order by id limit 1000")
+
+    val restbl =  tableEnv.sqlQuery("select * from testOne order by id limit 1000")
 
     //部分数据输出
     println("------------------------------------")
     tableEnv.toDataSet[Row](restbl).print()
 
-    //结果数据集写入到外部存储系统
-    val writeSink = new CsvTableSink("E:\\scala_workspace\\FlinkAllTest\\src\\main\\resources\\res100")
+
+    /***************************************
+      * 结果数据写入外部文件系统 function One
+      */
+    val writeSink = new CsvTableSink(".\\res100")
     restbl.writeToSink(writeSink)
+
+    /**
+      * 结果数据写入外部文件系统 function Two
+      */
+    restbl.toDataSet[(String,String,String,String,String)].setParallelism(1).writeAsCsv("res.csv","\n",",",WriteMode.OVERWRITE)
+
+
+
+
     batchenv.execute("table_test")
   }
 }
@@ -77,3 +109,4 @@ object TableAPITest {
 }
 
 case class DataPackage(id:Int,name:String,id2:Int) extends Serializable
+case class tableSchema(id:String,id2:String,id3:String,flag:String,number:String)
