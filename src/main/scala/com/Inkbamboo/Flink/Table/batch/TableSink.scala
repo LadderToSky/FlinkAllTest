29ff2706df994e90888772f241ac7fc291c12102
inkbamboo <hao_zl1104@163.com>
2019-08-12 22:38:40 +0800

    tableAPI代码结构添加sink部分

---
+++ src/main/scala/com/Inkbamboo/Flink/Table/batch/TableSink.scala
@@ -0,0 +1,13 @@
+package com.Inkbamboo.Flink.Table.batch
+
+/**
+  * Author: inkbamboo
+  * Date:   2019/8/12 22:38
+  *
+  * Think Twice, Code Once! 
+  *
+  * Desc:   TableApiSink操作
+  */
+class TableSink {
+
+}
