1b063c6a36630284417ef87d5e5e8f7ed52b6147
inkbamboo <hao_zl1104@163.com>
2019-08-24 15:17:29 +0800

    hdfs数据源，以及TableApi部分测试

--- src/main/scala/com/Inkbamboo/Flink/Table/batch/TableOperators.scala
+++ src/main/scala/com/Inkbamboo/Flink/Table/batch/TableOperators.scala
@@ -13,6 +13,12 @@ import org.apache.flink.types.Row
   */
 class TableOperators {
 
+
+  /**
+    * sql查询数据
+    * @param tblEnv
+    * @param tblNm
+    */
   def dataQuery(tblEnv:BatchTableEnvironment, tblNm:String): Unit ={
 
     val resData = tblEnv.sqlQuery(
@@ -20,8 +26,21 @@ class TableOperators {
         |select * from $tblNm
       """.stripMargin)
 
+
+
+    import org.apache.flink.table.api.scala._
+    val groubyData = resData
+                      //查询指定字段
+                      .select('user_id,'act_type,'timestamp)
+                      //根据act_type 分组
+                      .groupBy('act_type)
+                      //将分组后的数据计数统计
+                      .select('act_type,'timestamp.sum as 'sum2)
+
+     //输出表的schema信息
+     groubyData.printSchema()
+    //转换为dataset数据并输出
     import org.apache.flink.api.scala._
-    tblEnv.toDataSet[Row](resData)
-      .print()
+    groubyData.toDataSet[Row].print()
   }
 }
--- src/main/scala/com/Inkbamboo/Flink/Table/batch/TableSources.scala
+++ src/main/scala/com/Inkbamboo/Flink/Table/batch/TableSources.scala
@@ -1,9 +1,13 @@
 package com.Inkbamboo.Flink.Table.batch
 
+import java.util
+
 import com.Inkbamboo.beans.TableSourceBean
 import org.apache.flink.api.scala.ExecutionEnvironment
 import org.apache.flink.table.api.{Table, TableEnvironment}
 import org.apache.flink.table.api.scala.BatchTableEnvironment
+import org.apache.flink.table.catalog.{ExternalCatalog, ExternalCatalogTable}
+import org.apache.flink.table.descriptors.Kafka
 
 
 /**
@@ -18,6 +22,7 @@ class TableSources {
 
   /**
     * 从数据集合中加载数据源
+    * 从dataset中获取数据注册成表
     * 注册成表
     * @param tblenv
     * @param env
@@ -41,14 +46,42 @@ class TableSources {
       //加载csv文件
       //val csvds:DataSet[(String,String,String,String,String)] = env.readCsvFile("target/classes/UserBehavior.csv")
       //读取hdfs上的csv文件注册成表
-      val csvds:DataSet[(String,String,String,String,String)] = env.readCsvFile("hdfs:///zh/csvSinkTable/csvSinkTable.csv")
-      tblenv.registerDataSet("CsvTable",csvds,'id,'id2,'id3,'flag,'number)
+      val csvds:DataSet[(String,String,String,String,Long)] = env.readCsvFile("hdfs:///zh/csvSinkTable/csvSinkTable.csv")
+      tblenv.registerDataSet("CsvTable",csvds,'user_id,'pro_id,'pro_type_id,'act_type,'timestamp)
 
     }
 
   /**
-    *
+    *自定义注册外部数据源
+    * 以mysql数据源为例
     */
+  def sourceFromMysql(tblenv:BatchTableEnvironment,env:ExecutionEnvironment): Unit ={
+
+  }
 
 
 }
+
+/**
+  * 自定义mysql数据源作为table的外部数据源
+  *
+  *
+  */
+/*class externalSourceMysql extends ExternalCatalog{
+  override def getTable(tableName: String): ExternalCatalogTable ={
+      return ExternalCatalogTable.builder((new Kafka)).asTableSink()
+  }
+
+
+  override def listTables(): util.List[String] = {
+
+  }
+
+  override def getSubCatalog(dbName: String): ExternalCatalog = {
+
+  }
+
+  override def listSubCatalogs(): util.List[String] = {
+
+  }
+}*/
